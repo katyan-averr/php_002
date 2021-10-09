bca1c57700170c2ab9db5d6835c19f4c0b485046
katyan-averr <85882133+katyan-averr@users.noreply.github.com>
2021-10-09 21:49:46 +0800

    proverka

--- public/index.php
+++ public/index.php
@@ -1,15 +1,17 @@
 <?php
 
-// подключаем пакеты которые установили через composer
-require_once '../vendor/autoload.php';
-
-// создаем загрузчик шаблонов, и указываем папку с шаблонами
-// \Twig\Loader\FilesystemLoader -- это типа как в C# писать Twig.Loader.FilesystemLoader, 
-// только слеш вместо точек
-$loader = new \Twig\Loader\FilesystemLoader('../views');
-
-// создаем собственно экземпляр Twig с помощью которого будет рендерить
-$twig = new \Twig\Environment($loader);
-
 $url = $_SERVER["REQUEST_URI"];
+
+if ($url == "/") {
+
+    
+    echo $twig->render("main.html");
+} elseif (preg_match("#/nami#", $url)) {
+
+    
+    echo $twig->render("nami.html");
+} elseif (preg_match("#/robin#", $url)) {
+    
+    echo $twig->render("robin.html");
+}
 ?>
